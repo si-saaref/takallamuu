d16b98cd32b1c4c7bdc89037f2afa364e74af609
si-saaref <achmadsyarif241@gmail.com>
2023-06-16 08:10:37 +0700

    feat: slicing base page for login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import './styles/styles.css';
+import LoginPage from './pages/Login.Page';
 
 function App() {
 	return (
 		<>
 			<Routes>
 				<Route path='/' element={<h1>HOMEPAGE </h1>} />
-				<Route path='/login' element={<h1>LOGINPAGE </h1>} />
+				<Route path='/login' element={<LoginPage />} />
 				<Route path='/register' element={<h1>REGISTERPAGE </h1>} />
 			</Routes>
 		</>
---
+++ src/pages/Login.Page.jsx
@@ -0,0 +1,12 @@
+export default function LoginPage() {
+	return (
+		<>
+			<div className='loginpage container'>
+				<div className='container__inner'>
+					<h1>LOGIN PAGE</h1>
+					{/* <LoginForm /> */}
+				</div>
+			</div>
+		</>
+	);
+}
