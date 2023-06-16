9a143685e728f7935ca790102d8d9801c84541f5
si-saaref <achmadsyarif241@gmail.com>
2023-06-16 10:03:46 +0700

    feat: slicing header component

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import './styles/styles.css';
 import LoginPage from './pages/Login.Page';
+import Header from './components/organisms/Header';
 
 function App() {
 	return (
 		<>
-			<Routes>
-				<Route path='/' element={<h1>HOMEPAGE </h1>} />
-				<Route path='/login' element={<LoginPage />} />
-				<Route path='/register' element={<h1>REGISTERPAGE </h1>} />
-			</Routes>
+			<Header />
+			<main>
+				<Routes>
+					<Route path='/' element={<h1>HOMEPAGE </h1>} />
+					<Route path='/login' element={<LoginPage />} />
+					<Route path='/register' element={<h1>REGISTERPAGE </h1>} />
+				</Routes>
+			</main>
 		</>
 	);
 }
---
+++ src/components/organisms/Header.jsx
@@ -0,0 +1,15 @@
+export default function Header() {
+	return (
+		<>
+			<header className='header'>
+				<div className='header-inner'>
+					<h1>Takallamuu</h1>
+					<div className='header-menu'>
+						{/* <RiDiscussLine onClick={() => navigate('/')} />
+						<TbLogout onClick={onSignOut} /> */}
+					</div>
+				</div>
+			</header>
+		</>
+	);
+}
