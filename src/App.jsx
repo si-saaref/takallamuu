145d257261ad1656c9066eb251de361382360dd1
si-saaref <achmadsyarif241@gmail.com>
2023-06-18 11:25:54 +0700

    feat: integrate ispreload serivce with ui

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,17 @@ import DetailPage from './pages/DetailPage';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/Login.Page';
 import './styles/styles.css';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { asyncSetIsPreload } from './states/isPreload/action';
 
 function App() {
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(asyncSetIsPreload());
+	}, [dispatch]);
+
 	return (
 		<>
 			<main>
