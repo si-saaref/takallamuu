1eb3235b55dd7c197e1279347ae6ddf4b02c9e46
si-saaref <achmadsyarif241@gmail.com>
2023-06-19 10:58:54 +0700

    feat: configure store show more tags action

--- src/states/tags/action.js
+++ src/states/tags/action.js
@@ -1,5 +1,6 @@
 export const actionType = {
 	GET_ALL_TAGS: 'GET_ALL_TAGS',
+	SHOW_MORE_TAGS: 'SHOW_MORE_TAGS',
 };
 
 export const getAllTags = (tags) => {
@@ -17,6 +18,27 @@ export const asyncGetAllTags = () => {
 			const { threads } = getState();
 			const listTags = threads.map((item) => item?.category).slice(0, 7);
 			console.log('list hread +>', listTags);
+			dispatch(getAllTags(listTags));
+		} catch (error) {
+			console.log(error);
+		}
+	};
+};
+
+export const showMoreTags = (tags) => {
+	return {
+		type: actionType.GET_ALL_TAGS,
+		payload: {
+			tags,
+		},
+	};
+};
+
+export const asyncShowMoreTags = () => {
+	return (dispatch, getState) => {
+		try {
+			const { threads } = getState();
+			const listTags = threads.map((item) => item?.category);
 			dispatch(getAllTags(listTags));
 		} catch (error) {
 			console.log(error);
--- src/states/tags/reducer.js
+++ src/states/tags/reducer.js
@@ -5,6 +5,9 @@ export default function tagsReducer(state = [], action = {}) {
 		[actionType.GET_ALL_TAGS]: () => {
 			return action.payload.tags;
 		},
+		[actionType.SHOW_MORE_TAGS]: () => {
+			return action.payload.tags;
+		},
 		DEFAULT: () => state,
 	};
 
