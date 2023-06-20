ecaf0d8f2b5329045cc9795853b85f5ad1ccf460
si-saaref <achmadsyarif241@gmail.com>
2023-06-20 10:35:12 +0700

    perf: optimize get all tags

--- src/states/tags/action.js
+++ src/states/tags/action.js
@@ -19,13 +19,20 @@ export const asyncGetAllTags = () => {
 	return async (dispatch, getState) => {
 		try {
 			const { threads } = getState();
-			const tagsFromStorage = apiServices.getFromStorage('listTags');
+			const tagsFromStorage = apiServices.getFromStorage('listTags') || [];
 			const listTags =
-				tagsFromStorage.length === 0
-					? threads.map((item) => item?.category).slice(0, 7)
+				tagsFromStorage?.length === 0 || threads.length > tagsFromStorage?.length
+					? [...new Set(threads.map((item) => item?.category))].slice(0, 7)
 					: tagsFromStorage;
 
 			apiServices.putToStorage({ keyName: 'listTags', item: listTags });
+
+			console.log(
+				listTags,
+				tagsFromStorage?.length === 0,
+				threads.length > tagsFromStorage?.length,
+				tagsFromStorage
+			);
 
 			dispatch(getAllTags(listTags));
 		} catch (error) {
