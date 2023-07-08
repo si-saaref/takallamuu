9fc5c97795bef942331ed1830ee0d62927c23544
si-saaref <achmadsyarif241@gmail.com>
2023-07-08 08:32:22 +0700

    feat: fix ci test gh action

--- src/components/molecules/Loader/index.jsx
+++ src/components/molecules/Loader/index.jsx
@@ -3,7 +3,7 @@ import LoadingBar from 'react-redux-loading-bar';
 export default function Loader() {
 	return (
 		<div className='loading'>
-			<LoadingBar />
+			<LoadingBar className='load-bar' data-cy='loading-bar' />
 			{/* <LoadingBar updateTime={100} maxProgress={95} progressIncrease={10} /> */}
 		</div>
 	);
